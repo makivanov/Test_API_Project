6e1547a9e35e5aa9943a854731042c616b4b62c4
Maksim Ivanov <maksimivanov.06@yandex.ru>
2024-11-19 18:50:29 +1000

    added class for check status

--- src/check_status.py
+++ src/check_status.py
@@ -4,6 +4,7 @@ import allure
 class CheckStatus:
     def __init__(self):
         self.response = None
+
     def check_that_status_is_value(self, value):
         assert self.response.status_code == value
 
@@ -14,3 +15,19 @@ class CheckStatus:
     @allure.step('Проверка что код ответа 400')
     def check_that_status_is_400(self):
         self.check_that_status_is_value(400)
+
+    @allure.step('Проверка что код ответа 401')
+    def check_that_status_is_401(self):
+        self.check_that_status_is_value(401)
+
+    @allure.step('Проверка что код ответа 403')
+    def check_that_status_is_403(self):
+        self.check_that_status_is_value(403)
+
+    @allure.step('Проверка что код ответа 404')
+    def check_that_status_is_404(self):
+        self.check_that_status_is_value(404)
+
+    @allure.step('Проверка что код ответа 405')
+    def check_that_status_is_405(self):
+        self.check_that_status_is_value(405)
