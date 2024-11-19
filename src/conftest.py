39306d30b5a992aa6e23a0499756603a67f32679
Maksim Ivanov <maksimivanov.06@yandex.ru>
2024-11-19 18:48:40 +1000

    edited conftest

---
+++ src/conftest.py
@@ -0,0 +1,53 @@
+import random
+
+import pytest
+
+from endpoints.add_meme import AddMeme
+from endpoints.autorization import Authorization
+from endpoints.delete_meme import DeleteMeme
+from endpoints.get_one_meme import GetMeme
+from endpoints.update_put_meme import PutMeme
+from test_data.authorization_test_data import AUTHORIZATION_NAME
+
+
+@pytest.fixture(scope="session")
+def authorization(test_name=AUTHORIZATION_NAME):
+    name = random.choice(test_name)
+    return Authorization(name)
+
+@pytest.fixture()
+def token(authorization):
+    return authorization.get_token()
+
+
+@pytest.fixture()
+def create_meme(token):
+    meme = AddMeme(token=token)
+    yield meme
+    meme_for_delete = DeleteMeme(token=token)
+    if meme.meme_id:
+        meme_for_delete.delete_meme(meme.meme_id)
+
+@pytest.fixture()
+def get_meme(token):
+    meme = GetMeme(token=token)
+    yield meme
+    meme_for_delete = DeleteMeme(token=token)
+    if meme.meme_id:
+        meme_for_delete.delete_meme(meme.meme_id)
+
+
+@pytest.fixture()
+def change_meme(token):
+    meme = PutMeme(token=token)
+    yield meme
+    meme_for_delete = DeleteMeme(token=token)
+    if meme.meme_id:
+        meme_for_delete.delete_meme(meme.meme_id)
+
+@pytest.fixture()
+def delete_meme(token):
+    meme = DeleteMeme(token=token)
+    yield meme
+    if meme.meme_id:
+        meme.delete_meme(meme.meme_id)
